b2ac30588e315bcc2d3ee591924a86dd56a43594
Darv <86darv@gmail.com>
2025-05-15 20:55:16 +0300

    assert

--- main.test.js
+++ main.test.js
@@ -2,4 +2,19 @@ import assert from 'node:assert';
 
 assert.ok(1 === 1);
 assert.equal(1, 1);
-assert.notEqual(1, 0);
+assert.notEqual(1, 0, 'error message (available for all assert methods');
+
+// pass
+// assert.equal('', false);
+// fail
+// assert.strictEqual('', false);
+
+// testing regex
+// assert.match();
+// assert.doesNotMatch();
+
+// testing objects
+// assert.deepEqual(actual, expected);
+// assert.deepStrictEqual(actual, expected);
+// assert.notDeepEqual(actual, expected);
+// assert.notDeepStrictEqual(actual, expected);
